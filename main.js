4dfa96f08dea255be0948c9cc77ea2bdc34054f6
robdcampbell <rdeancampbell@gmail.com>
2020-10-12 20:05:12 -0400

    Finish Loan calculator UX, Error handling. App Complete

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+document.getElementById('loan-form').addEventListener('submit',function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  // Show loader
+  document.getElementById('loading').style.display= 'block';
+  setTimeout(calculateResults,2000)
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e){
+function calculateResults(){
   console.log('Calculating...')
 
   // UI Vars
@@ -26,17 +33,49 @@ function calculateResults(e){
   
   
   if(isFinite(monthly)){
-
-    console.log('Pterodactyl')
-    
     
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
-    
-  } else{
-   console.log('Please check your numbers')
-  }
 
-  e.preventDefault();
+    // Show results
+    document.getElementById('results').style.display = 'block';
+    // Hide Loader
+    document.getElementById('loading').style.display = 'none';
+  } else{
+   showError('Please check your numbers');
+  }
+}
+
+
+// Show Error 
+function showError(error){
+
+   // Hide results
+   document.getElementById('results').style.display = 'none';
+   // Hide Loader
+   document.getElementById('loading').style.display = 'none';
+
+  // Create a div
+  const errorDiv = document.createElement('div');
+  
+  // Get Elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+  //Create text node and apend to div
+  errorDiv.appendChild(document.createTextNode(error));
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear Error 
+function clearError(){
+  document.querySelector('.alert').remove();
 }
